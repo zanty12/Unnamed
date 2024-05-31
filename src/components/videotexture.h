87182dab09c3380c1d9b228c48cc6a8eae2b4dfe
Cheung To Hung <54949055+zanty12@users.noreply.github.com>
2024-05-31 14:32:47 +0900

    we dont need 10 frames for buffering

--- src/components/videotexture.h
+++ src/components/videotexture.h
@@ -104,7 +104,7 @@ public:
         time_ += Time::GetDeltaTime();
 
         // Define a constant for the maximum buffer size.
-        const int MAX_BUFFER_SIZE = 10; // Change this value as needed.
+        const int MAX_BUFFER_SIZE = 2; // Change this value as needed.
 
         int64_t pts = 0;
         // Check if the buffer is not full before loading frame data.
